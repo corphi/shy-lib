e09ce4f0a9d41a30cb40432a34cc310dc7eaee90
Philipp Cordes <pc@irgendware.net>
2012-12-21 05:26:15 +0100

    Streamlining the autoloader.

--- autoloader.php
+++ autoloader.php
@@ -13,9 +13,7 @@ function autoloader($name)
 	if (substr($name, 0, 4) !== 'Shy\\') {
 		return false;
 	}
-
-	$name = substr($name, 3);
-	include __DIR__ . str_replace('\\', '/', $name) . '.php';
+	return include(__DIR__ . '/' . str_replace('\\', '/', substr($name, 4)) . '.php');
 }
 
-spl_autoload_register('Shy\autoloader');
+spl_autoload_register('Shy\\autoloader');
